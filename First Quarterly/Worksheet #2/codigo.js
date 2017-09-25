842c4172182da9d9adb86301a5e0cfc0f5306c8f
IhToN <ihton93@gmail.com>
2017-09-25 08:48:08 +0200

    Worksheet #1 - 4 Worksheet #2 - 3, 4

---
+++ First Quarterly/Worksheet #2/codigo.js
@@ -0,0 +1,10 @@
+// ej3
+var radius = window.prompt("Introduce el radio de un círculo");
+console.log("The circumference is " + (2 * Math.PI * radius));
+console.log("The area is " + (Math.PI * (radius ^ 2)));
+
+// ej4
+var celsius = window.prompt("Introduce la temperatura en Celsius");
+console.log(celsius + "ºC is " + (celsius * (9 / 5) + 32) + "ºF");
+var fahrenheit = window.prompt("Introduce la temperatura en Fahrenheit");
+console.log(fahrenheit + "ºC is " + ((fahrenheit - 32) / (9 / 5)) + "ºF");
