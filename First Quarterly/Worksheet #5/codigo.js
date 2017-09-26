2a2d56560589e673b7998deb56eb81be617e8a01
IhToN <ihton93@gmail.com>
2017-09-26 09:47:32 +0200

    Worksheet #5 - 4.11, 4.12 fix

--- First Quarterly/Worksheet #5/codigo.js
+++ First Quarterly/Worksheet #5/codigo.js
@@ -9,27 +9,44 @@ document.write("<br /><h1>Práctica del bucle for</h1>");
 cabeceras();
 
 // ej4.11
-function tablaBlanca(columnas) {
-    document.write("<table border='0' cellspacing='2' bgcolor='black' width='200'>");
+function pintarTabla(blanca) {
+    let col = window.prompt("Introduce el número de columnas");
+    let anc = window.prompt("Introduce el ancho de las celdas");
+    let alt = window.prompt("Introduce el alto de las celdas");
+    if (!Number.isInteger(col) || !Number.isInteger(anc) || !Number.isInteger(alt)) {
+        document.write("Has introducido algún valor incorrecto:<br /><ul>" +
+            "<li>Nº Columnas: " + col + "</li>" +
+            "<li>Ancho Columnas: " + anc + "</li>" +
+            "<li>Alto Columnas: " + alt + "</li></ul>");
+        return;
+    }
+    if (blanca)
+        pintarTablaBlanca(col, anc, alt);
+    else
+        pintarTablaBlancaNegra(col, anc, alt);
+}
+
+function pintarTablaBlanca(columnas, ancho, alto) {
+    document.write("<table border='0' cellspacing='2' bgcolor='black' width='\"+(ancho * columnas)+\"'>");
     document.write("<tr bgcolor='white'>");
     for (var i = 1; i <= columnas; i++)
-        document.write("<td width='50' height='50'>&nbsp;</td>")
+        document.write("<td width='" + ancho + "' height='" + alto + "'>&nbsp;</td>")
     document.write("</tr>");
     document.write("</table>");
 }
 
 document.write("<br /><h1>Práctica de la Tabla</h1>");
-tablaBlanca(4);
+pintarTabla(true);
 
 // ej4.12
-function tablaBlancoNegro(columnas) {
-    document.write("<table border='0' cellspacing='2' bgcolor='black' width='200'>");
+function pintarTablaBlancaNegra(columnas, ancho, alto) {
+    document.write("<table border='0' cellspacing='2' bgcolor='black' width='" + (ancho * columnas) + "'>");
     document.write("<tr bgcolor='white'>");
     for (var i = 1; i <= columnas; i++)
-        document.write("<td width='50' height='50' bgcolor='" + ((i % 2 === 0) ? 'white' : 'black') + "'>&nbsp;</td>")
+        document.write("<td width='" + ancho + "' height='" + alto + "' bgcolor='" + ((i % 2 === 0) ? 'white' : 'black') + "'>&nbsp;</td>")
     document.write("</tr>");
     document.write("</table>");
 }
 
 document.write("<br /><h1>Práctica de la Tabla en Blanco y Negro</h1>");
-tablaBlancoNegro(4);
+pintarTabla(false);
