38931060278d403894a715c5b3d2b440df2a5941
IhToN <ihton93@gmail.com>
2017-09-29 13:27:46 +0200

    Unit 2 - Worksheet 2 - in process

--- First Quarterly/Unit 2/Worksheet #2/codigo.js
+++ First Quarterly/Unit 2/Worksheet #2/codigo.js
@@ -10,3 +10,28 @@ var getRandom = (min, max) => {
 console.log(getRandom(1000, 2000));
 
 // 5
+var funcionSegundoGrado = (a, b, c) => {
+    let sol1 = (-b + (Math.sqrt((b ** 2) - 4 * a * c))) / (2 * a);
+    let sol2 = (-b - (Math.sqrt((b ** 2) - 4 * a * c))) / (2 * a);
+    return [sol1, sol2];
+};
+console.log(funcionSegundoGrado(3, 4, 5));
+
+// 7
+var senosHasta = (numero) => {
+    document.write("<table>");
+    for (let i = 0; i <= numero; i++) {
+        document.write("<tr>");
+        document.write("<td>" + $i + "</td>");
+        document.write("<td>" + Math.sin($i * (180 / Math.PI)) + "</td>");
+        document.write("</tr>");
+    }
+    document.write("</table>");
+};
+
+senosHasta(180);
+
+// 8
+var imagenAleatoria = () => {
+    let imagenes = ['https://lh3.googleusercontent.com/-VHV916run58/WC9To_x72tI/AAAAAAAACkE/f59cQ9_9-XY/safe_image_thumb.gif?imgmax=800']
+}
