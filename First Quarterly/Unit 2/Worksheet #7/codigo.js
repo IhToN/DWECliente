b58299cb9306240d0fc579084185bfdc8111cc69
IhToN <ihton93@gmail.com>
2017-10-05 14:44:27 +0200

    Unit 2 - Worksheet #7 - Almost complete

---
+++ First Quarterly/Unit 2/Worksheet #7/codigo.js
@@ -0,0 +1,94 @@
+// 1
+var toRad = (x) => x * Math.PI / 180;
+
+var calcDistance = (p1, p2) => { // Points are Geolocation.coords objects
+    var R = 6371; // earth's mean radius in km
+    var dLat = toRad(p2.latitude - p1.latitude);
+    var dLong = toRad(p2.longitude - p1.longitude);
+
+    var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos(toRad(p1.latitude)) * Math.cos(toRad(p2.latitude)) * Math.sin(dLong / 2) * Math.sin(dLong / 2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    var d = R * c;
+
+    return d.toFixed(3);
+}
+
+var exerOne = () => {
+    if ("geolocation" in navigator) {
+        var lastPosition;
+        var distance = 0;
+        navigator.geolocation.getCurrentPosition(function (position) {
+            console.log('Lat: ' + position.coords.latitude + '|| Lon: ' + position.coords.longitude);
+            lastPosition = position;
+        });
+
+        var watchID = navigator.geolocation.watchPosition(function (position) {
+            console.log('New!!  Lat: ' + position.coords.latitude + '|| Lon: ' + position.coords.longitude);
+            distance += calcDistance(lastPosition.coords, position.coords);
+            lastPosition = position;
+        });
+
+    } else console.log('Sorry, we couldn\'t get your position.');
+};
+exerOne();
+
+// 2
+var marker;
+
+var geocodeLatLng = (geocoder, map, infowindow, coords) => {
+    var latlng = {lat: coords.latitude, lng: coords.longitude};
+    geocoder.geocode({'location': latlng}, function(results, status) {
+        if (status === 'OK') {
+            if (results[1]) {
+                infowindow.setContent(results[1].formatted_address);
+                infowindow.open(map, marker);
+            } else {
+                window.alert('No results found');
+            }
+        } else {
+            window.alert('Geocoder failed due to: ' + status);
+        }
+    });
+};
+
+var drawMap = (position) => {
+    var uluru = {lat: position.coords.latitude, lng: position.coords.longitude};
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 16,
+        center: uluru
+    });
+    marker = new google.maps.Marker({
+        position: uluru,
+        map: map,
+        animation: google.maps.Animation.DROP,
+        draggable: true,
+    });
+
+    marker.addListener('click', toggleBounce);
+
+    var geocoder = new google.maps.Geocoder;
+    var infowindow = new google.maps.InfoWindow;
+
+    geocodeLatLng(geocoder, map, infowindow, position.coords);
+};
+
+function toggleBounce() {
+    if (marker.getAnimation() !== null) {
+        marker.setAnimation(null);
+    } else {
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+    }
+};
+
+var gmapApi = () => {
+    if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            drawMap(position);
+        });
+
+        var watchID = navigator.geolocation.watchPosition(function (position) {
+            marker.position = {lat: position.coords.latitude, lng: position.coords.longitude};
+        });
+    } else console.log('Sorry, we couldn\'t get your position.');
+};
