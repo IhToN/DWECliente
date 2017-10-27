6c147689e8e3521f40d38ebe08d037e2153ace8a
IhToN <ihton93@gmail.com>
2017-10-27 10:26:13 +0200

    Ejercicios pizarra - 1 y 2

---
+++ First Quarterly/Unit 4/Ejercicios Pizarra/codigo.js
@@ -0,0 +1,11 @@
+// 1 y 2
+document.getElementById("touchstart", function (event) {
+    let html_str = '<ul>';
+    event.touches.forEach((touch, idx) => {
+        str += "<li>" + idx + ". (" + touch.x + ", " + touch.y + ")</li>";
+    });
+    html_str += '</ul>';
+    document.getElementById("coords").innerHTML = str
+}, false);
+
+// 3 - can't touch this
