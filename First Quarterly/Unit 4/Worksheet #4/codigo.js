80529ca735cca1f2cbe80d5a2b5b7359a7329885
IhToN <ihton93@gmail.com>
2017-11-02 12:28:35 +0100

    Unit 4 - Worksheet 4

--- First Quarterly/Unit 4/Worksheet #4/codigo.js
+++ First Quarterly/Unit 4/Worksheet #4/codigo.js
@@ -1,33 +1,49 @@
-// 1
+// 1 y 2
 var saludo = () => {
-    if (!document.cookie) {
+    let nameck = '';
+    let bgck = '';
+    let bdck = '';
+    let txtck = '';
+    if(document.cookie) {
+        let cookie = document.cookie.split(';');
+        nameck = cookie[0].split('=')[1];
+        bgck = cookie[1].split('=')[1];
+        bdck = cookie[2].split('=')[1];
+        txtck = cookie[3].split('=')[1];
+    }
+    if (!document.cookie || nameck === '') {
         document.getElementById('submit').addEventListener('click', () => {
             let name = document.getElementById('username');
+            let fondo = document.getElementById('colorfondo');
+            let border = document.getElementById('colorborder');
+            let text = document.getElementById('colortext');
             let expires = new Date(new Date().getTime() + 5 * 60000);
-            console.log(name.value);
-            console.log(expires.toDateString());
-            document.cookie = 'username=' + name.value + '; path=/; ' + '; expires=' + expires.toDateString();
-            console.log(document.cookie);
+            document.cookie = 'username=' + name.value + '; path=/; ' + '; expires=' + expires.toUTCString();
+            document.cookie = 'colorfondo=' + fondo.value + '; path=/; ' + '; expires=' + expires.toUTCString();
+            document.cookie = 'colorborder=' + border.value + '; path=/; ' + '; expires=' + expires.toUTCString();
+            document.cookie = 'colortext=' + text.value + '; path=/; ' + '; expires=' + expires.toUTCString();
+            location.reload();
         });
     } else {
-        console.log(document.cookie);
         let bloque = document.getElementById('hello');
-        let firstCookie = document.cookie.split(';')[0];
-        let name = firstCookie.split('=')[1];
-        bloque.innerHTML = 'Hola <strong>' + name + '</strong>!<br/>' +
+
+        bloque.innerHTML = 'Hola <strong>' + nameck + '</strong>!<br/>' +
             '<button id="delete">Borrar Cookie</button>';
+        bloque.style.backgroundColor = bgck;
+        bloque.style.border = "2px solid " + bdck;
+        bloque.style.color = txtck;
 
         document.getElementById('delete').addEventListener('click', () => {
-            console.log('clikao 2');
-            document.cookie = '';
-            console.log(document.cookie);
+            document.cookie = 'username=; path=/; expires=Wed; 01 Jan 1970';
+            document.cookie = 'colorfondo=; path=/; expires=Wed; 01 Jan 1970';
+            document.cookie = 'colorborder=; path=/; expires=Wed; 01 Jan 1970';
+            document.cookie = 'colortext=; path=/; expires=Wed; 01 Jan 1970';
+            location.reload();
         });
     }
 };
 
 window.addEventListener("load", saludo);
 
-// 2
-
-
 // 3
+// expires vacío, se crea como cookie de sesión y se borra solita.
