cdb4c1384bfbbc48a1c94452770c9f79598dc70b
IhToN <ihton93@gmail.com>
2017-11-03 10:40:27 +0100

    U5WS1 - 1 y 4

--- First Quarterly/Unit 5/Worksheet #1/codigo.js
+++ First Quarterly/Unit 5/Worksheet #1/codigo.js
@@ -1,10 +1,21 @@
 // 1
-
+let allLinks = document.getElementsByTagName('a');
+console.log("Enlaces totales: " + allLinks.length);
+console.log("Link del penúltimo enlace: " + allLinks.length > 2 ? allLinks[allLinks.length - 2].href : allLinks[allLinks.length - 1].href);
+console.log("Enlaces totales a Google: " + document.querySelectorAll("a[href*='google']").length);
+console.log("Enlaces totales del tercer párrafo: " + document.querySelectorAll("p:nth-child(4) a").length);
 
 // 4
-
+let addelement = () => {
+    let list = document.querySelector("ul.list");
+    let newEntry = document.createElement('li');
+    newEntry.appendChild(document.createTextNode("Elemento " + (list.childNodes.length)));
+    list.appendChild(newEntry);
+};
+document.getElementById("addelement").addEventListener("click", addelement);
 
 // 5
+
 
 
 // 6
