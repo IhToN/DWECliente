12744cd6f70296f72f0b5cae222e51dbfe95b153
IhToN <ihton93@gmail.com>
2017-11-06 12:30:27 +0100

    Pelotica rebotando

--- First Quarterly/Unit 5/Canvas Game/codigo.js
+++ First Quarterly/Unit 5/Canvas Game/codigo.js
@@ -17,9 +17,11 @@ var bajando = true;
 var derechando = true;
 let draw = () => {
     let pelota = document.getElementById('pelota');
-    let movx = 1;
-    pelota.setAttribute('cx', (parseInt(pelota.getAttribute('cx')) + (derechando ? 3 : -3)).toString());
-    pelota.setAttribute('cy', (parseInt(pelota.getAttribute('cy')) + (bajando ? 3 : -3)).toString());
+    let movx = 3, movy = 3;
+
+    pelota.setAttribute('cx', (parseInt(pelota.getAttribute('cx')) + (derechando ? movx : -movx)).toString());
+    pelota.setAttribute('cy', (parseInt(pelota.getAttribute('cy')) + (bajando ? movy : -movy)).toString());
+
     if(pelota.getAttribute('cx') >= 1050) derechando = false;
     else if (pelota.getAttribute('cx') <= 50) derechando = true;
     if(pelota.getAttribute('cy') >= 650) bajando = false;
