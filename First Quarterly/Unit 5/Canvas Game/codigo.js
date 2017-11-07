50dc87b68450063cd0b9f61b1d64dc1128959b34
IhToN <ihton93@gmail.com>
2017-11-07 12:37:53 +0100

    Multiballs - detecting collisions

--- First Quarterly/Unit 5/Canvas Game/codigo.js
+++ First Quarterly/Unit 5/Canvas Game/codigo.js
@@ -13,19 +13,36 @@ let loop = () => {
     draw();
 };
 
-var bajando = true;
-var derechando = true;
 let draw = () => {
-    let pelota = document.getElementById('pelota');
-    let movx = 3, movy = 3;
+    let pelotas = document.getElementsByClassName('pelota');
+    Array.prototype.forEach.call(pelotas, (pelota) => {
+        pelota.setAttribute('cx', (parseInt(pelota.getAttribute('cx')) + (pelota.derechando ? pelota.movx : -pelota.movx)).toString());
+        pelota.setAttribute('cy', (parseInt(pelota.getAttribute('cy')) + (pelota.bajando ? pelota.movy : -pelota.movy)).toString());
 
-    pelota.setAttribute('cx', (parseInt(pelota.getAttribute('cx')) + (derechando ? movx : -movx)).toString());
-    pelota.setAttribute('cy', (parseInt(pelota.getAttribute('cy')) + (bajando ? movy : -movy)).toString());
+        checkBorderCollision(pelota);
+        checkBallsCollision(pelotas, pelota);
+    });
+};
 
-    if(pelota.getAttribute('cx') >= 1050) derechando = false;
-    else if (pelota.getAttribute('cx') <= 50) derechando = true;
-    if(pelota.getAttribute('cy') >= 650) bajando = false;
-    else if (pelota.getAttribute('cy') <= 50) bajando = true;
+let checkBorderCollision = (pelota) => {
+    if (pelota.getAttribute('cx') >= 1050) pelota.derechando = false;
+    else if (pelota.getAttribute('cx') <= 50) pelota.derechando = true;
+    if (pelota.getAttribute('cy') >= 650) pelota.bajando = false;
+    else if (pelota.getAttribute('cy') <= 50) pelota.bajando = true;
+};
+
+let checkBallsCollision = (pelotas, pelota) => {
+    Array.prototype.forEach.call(pelotas, (plcheck) => {
+        if (plcheck !== pelota) {
+            let bndPlt = pelota.getBoundingClientRect();
+            let bndChk = plcheck.getBoundingClientRect();
+
+            if (!(bndChk.left > bndPlt.right ||
+                    bndChk.right < bndPlt.left ||
+                    bndChk.top > bndPlt.bottom ||
+                    bndChk.bottom < bndPlt.top)) console.log('Choque entre ' + pelota.getAttribute('fill') + ' y ' + plcheck.getAttribute('fill'));
+        }
+    });
 };
 
 let anim = () => {
@@ -33,4 +50,15 @@ let anim = () => {
     requestAnimFrame(anim);
 };
 
-anim();
+let init = () => {
+    let pelotas = document.getElementsByClassName('pelota');
+    Array.prototype.forEach.call(pelotas, (pelota) => {
+        pelota.bajando = true;
+        pelota.derechando = true;
+        pelota.movx = pelota.hasAttribute('movx') ? parseInt(pelota.getAttribute('movx')) : 3;
+        pelota.movy = pelota.hasAttribute('movy') ? parseInt(pelota.getAttribute('movy')) : 3;
+    });
+    anim();
+};
+
+init();
