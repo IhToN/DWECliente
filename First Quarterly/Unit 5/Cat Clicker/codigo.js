a0625bae8f4a448efc009449fb74d297de54c28d
IhToN <ihton93@gmail.com>
2017-11-09 14:32:44 +0100

    toclasses

--- First Quarterly/Unit 5/Cat Clicker/codigo.js
+++ First Quarterly/Unit 5/Cat Clicker/codigo.js
@@ -1,3 +1,23 @@
+let hasselhoffs = [];
+
+class Haselhoff {
+    constructor(id, src) {
+        this.id = id;
+        this.src = src;
+        this.clicks = 0;
+    }
+
+    getButton() {
+        return '<button class="catbutton" img-id="' + this.id + '">Hasselhoff #' + this.id + '</button>';
+    }
+
+    getHTML() {
+        let imageString = '<img img-id="' + this.id + '" img-clicks="' + this.clicks + '" class="catimage" src="' + this.src + '" />';
+        let text = '<br/><div class="cattext img' + this.id + '">David Hasselhoff #' + this.id + ' - Veces clickado: ' + this.clicks + '</div>';
+        return imageString + text;
+    }
+}
+
 let initCatClicker = () => {
     let catBoxes = document.getElementsByClassName("catboxes");
 
@@ -17,25 +37,44 @@ let initCatClicker = () => {
 let loadBoxes = () => {
     let catBoxes = document.getElementsByClassName("catbox");
     Array.prototype.forEach.call(catBoxes, (catbox, id) => {
-        let imageString = '<img img-id="' + id + '" img-clicks="0" class="catimage" src="http://place-hoff.com/40' + Math.floor(Math.random() * 9) + '/30' + Math.floor(Math.random() * 9) + '" />';
-        let text = '<br/><div class="cattext img' + id + '"></div>';
-
-        catbox.innerHTML += imageString;
-        catbox.innerHTML += text;
+        let hoff = new Haselhoff(id, 'http://place-hoff.com/40' + Math.floor(Math.random() * 9) + '/30' + Math.floor(Math.random() * 9));
+        hasselhoffs[id] = hoff;
+        catbox.innerHTML = hoff.getButton();
     });
 
     listenEvents();
 };
 
 let listenEvents = () => {
+    listenEventsButtons();
+    listenEventsImages();
+};
+
+let listenEventsButtons = () => {
+    let catButtons = document.getElementsByClassName("catbutton");
+    Array.prototype.forEach.call(catButtons, (catbutton) => {
+        catbutton.addEventListener("click", (evt) => {
+            let displaybox = document.getElementById('displaybox');
+
+            let imgid = evt.target.getAttribute("img-id");
+            let hoff = hasselhoffs[imgid];
+
+            displaybox.innerHTML = hoff.getHTML();
+            listenEventsImages();
+        });
+    });
+};
+
+let listenEventsImages = () => {
     let catImages = document.getElementsByClassName("catimage");
     Array.prototype.forEach.call(catImages, (catimage) => {
         catimage.addEventListener("click", (evt) => {
             let imgid = evt.target.getAttribute("img-id");
-            let imgclicks = parseInt(evt.target.getAttribute("img-clicks"));
+            let hoff = hasselhoffs[imgid];
+
             let text = document.querySelector(".cattext.img" + imgid);
-            evt.target.setAttribute("img-clicks", ++imgclicks + "");
-            text.innerHTML = "David Hasselhoff #" + imgid + " - Veces clickado: " + imgclicks;
+            evt.target.setAttribute("img-clicks", ++hoff.clicks + "");
+            text.innerHTML = "David Hasselhoff #" + imgid + " - Veces clickado: " + hoff.clicks;
         });
     });
 };
