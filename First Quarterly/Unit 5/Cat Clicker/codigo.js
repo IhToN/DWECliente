53eadab6a5277a703598d93e52fff1a266503eb5
IhToN <ihton93@gmail.com>
2017-11-09 13:48:38 +0100

    nImages modificable

--- First Quarterly/Unit 5/Cat Clicker/codigo.js
+++ First Quarterly/Unit 5/Cat Clicker/codigo.js
@@ -1,6 +1,21 @@
 let initCatClicker = () => {
+    let catBoxes = document.getElementsByClassName("catboxes");
+
+    if (catBoxes) {
+        Array.prototype.forEach.call(catBoxes, (catbox, id) => {
+            let nImages = catbox.hasAttribute("nimages") ? parseInt(catbox.getAttribute("nimages")) : 3;
+            for (let i = 0; i < nImages; i++) {
+                let div = document.createElement('div');
+                div.className = "catbox";
+                catbox.appendChild(div);
+            }
+        });
+        loadBoxes();
+    }
+};
+
+let loadBoxes = () => {
     let catBoxes = document.getElementsByClassName("catbox");
-    console.log(catBoxes);
     Array.prototype.forEach.call(catBoxes, (catbox, id) => {
         let imageString = '<img img-id="' + id + '" img-clicks="0" class="catimage" src="http://place-hoff.com/40' + Math.floor(Math.random() * 9) + '/30' + Math.floor(Math.random() * 9) + '" />';
         let text = '<br/><div class="cattext img' + id + '"></div>';
