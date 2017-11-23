ca6889682f8a9162ed7af16805efbfbd716d7f7f
IhToN <ihton93@gmail.com>
2017-11-23 14:35:59 +0100

    Private properties Exam

--- Practical Exams/U1_4 13_11_2017/codigo.js
+++ Practical Exams/U1_4 13_11_2017/codigo.js
@@ -31,30 +31,27 @@ let Ejercicio3 = (newTitle = undefined) => {
 //a
 class Gato {
     constructor(nombre, edad, tamanyo, raza) {
-        this._nombre = nombre;
-        this._edad = edad;
-        this._tamanyo = tamanyo;
-        this._raza = raza;
-    }
+        let pnombre = nombre;
+        let pedad = edad;
+        let ptamanyo = tamanyo;
+        let praza = raza;
 
-    get nombre() {
-        return this._nombre;
-    }
+        this.nombre = () => {
+            return pnombre;
+        };
+        this.edad = () => {
+            return pedad;
+        };
+        this.tamanyo = () => {
+            return ptamanyo;
+        };
+        this.raza = () => {
+            return praza;
+        };
 
-    get edad() {
-        return this._edad;
-    }
-
-    get tamanyo() {
-        return this._tamanyo;
-    }
-
-    get raza() {
-        return this._raza;
-    }
-
-    caracteristicas() {
-        return `Me llamo ${this._nombre}, tengo ${this._edad} años, mido ${this._tamanyo}cm y soy un ${this._raza}.`;
+        this.caracteristicas = () => {
+            return `Me llamo ${pnombre}, tengo ${pedad} años, mido ${ptamanyo}cm y soy un ${praza}.`;
+        }
     }
 }
 
@@ -71,7 +68,9 @@ class JaulaGatitos {
     }
 
     buscarGato(nombre = undefined) {
-        if (nombre) for (let gato of this._gatitos) if (gato.nombre === nombre) return gato.caracteristicas();
+        if (nombre) for (let gato of this._gatitos)
+            if (gato.nombre() === nombre)
+                return gato.caracteristicas();
         return "No lo encuentro";
     }
 
@@ -82,73 +81,86 @@ class JaulaGatitos {
     }
 }
 
-let testEj3 = () => {
-    let jaula = new JaulaGatitos();
+let
+    testEj3 = () => {
+        let jaula = new JaulaGatitos();
 
-    let g = new Gato("misifu", 2, 24, "siamés");
+        let g = new Gato("misifu", 2, 24, "siamés");
 
-    jaula.anyadirGato(g);
-    console.log(jaula.mostrarGatos());
-    console.log(jaula.buscarGato("misifu"));
-    console.log(jaula.buscarGato("pepeluí"));
-};
+        jaula.anyadirGato(g);
+        console.log(jaula.mostrarGatos());
+        console.log(jaula.buscarGato("misifu"));
+        console.log(jaula.buscarGato("pepeluí"));
+    };
 
 // 4 ====================
-let aplicaEstilos = (idTabla = undefined) => {
-    if (idTabla) {
-        let ths = document.querySelectorAll(`#${idTabla} th`);
-        let evenRow = document.querySelectorAll(`#${idTabla} tr:nth-child(even) td`);
-        let oddRow = document.querySelectorAll(`#${idTabla} tr:nth-child(odd) td`);
+let
+    aplicaEstilos = (idTabla = undefined) => {
+        if (idTabla) {
+            let ths = document.querySelectorAll(`#${idTabla} th`);
+            let evenRow = document.querySelectorAll(`#${idTabla} tr:nth-child(even) td`);
+            let oddRow = document.querySelectorAll(`#${idTabla} tr:nth-child(odd) td`);
 
-        Array.prototype.forEach.call(ths, function (th) {
-            th.style.color = 'red';
-        });
-        Array.prototype.forEach.call(evenRow, function (row) {
-            row.style.backgroundColor = 'blue';
-        });
-        Array.prototype.forEach.call(oddRow, function (row) {
-            row.style.backgroundColor = 'green';
-        });
+            Array.prototype.forEach.call(ths, function (th) {
+                th.style.color = 'red';
+            });
+            Array.prototype.forEach.call(evenRow, function (row) {
+                row.style.backgroundColor = 'blue';
+            });
+            Array.prototype.forEach.call(oddRow, function (row) {
+                row.style.backgroundColor = 'green';
+            });
 
-    }
-};
+        }
+    };
 
 // 5 ====================
-let wtTO;
-let clear = (evt) => {
-    evt.preventDefault();
-    evt.target.value = '';
-    let warn = document.getElementById('aviso');
-    if (warn) {
-        warn.innerHTML = '';
-        if (wtTO) clearTimeout(wtTO);
-    }
-};
+let
+    wtTO;
+let
+    clear = (evt) => {
+        evt.preventDefault();
+        evt.target.value = '';
+        let warn = document.getElementById('aviso');
+        if (warn) {
+            warn.innerHTML = '';
+            if (wtTO) clearTimeout(wtTO);
+        }
+    };
 
-let warn = () => {
-    let warnBox = document.getElementById('aviso');
-    if (warnBox) {
-        warnBox.innerHTML = 'está escribiendo...';
-        if (wtTO) clearTimeout(wtTO);
-        wtTO = setTimeout(() => {
-            warnBox.innerHTML = '';
-        }, 3 * 1000);
-    }
-};
+let
+    warn = () => {
+        let warnBox = document.getElementById('aviso');
+        if (warnBox) {
+            warnBox.innerHTML = 'está escribiendo...';
+            if (wtTO) clearTimeout(wtTO);
+            wtTO = setTimeout(() => {
+                warnBox.innerHTML = '';
+            }, 3 * 1000);
+        }
+    };
 
-let writing = (evt) => {
-    if (evt.keyCode === 13) {
-        clear(evt);
-    } else {
-        warn();
-    }
-};
+let
+    writing = (evt) => {
+        if (evt.keyCode === 13) {
+            clear(evt);
+        } else {
+            warn();
+        }
+    };
 
-let initWhatsapp = () => {
-    let box = document.getElementById('mensaje');
-    if (box) {
-        box.addEventListener('keypress', writing);
-    }
-};
+let
+    initWhatsapp = () => {
+        let box = document.getElementById('mensaje');
+        if (box) {
+            box.addEventListener('keypress', writing);
+        }
+    };
 
-window.addEventListener('load', initWhatsapp);
+window
+    .addEventListener(
+        'load'
+        ,
+        initWhatsapp
+    )
+;
