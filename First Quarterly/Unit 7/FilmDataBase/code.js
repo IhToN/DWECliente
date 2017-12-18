c89ac5b55c8468f9d54acb9e356edecc52ca2aa9
IhToN <ihton93@gmail.com>
2017-12-18 18:50:36 +0100

    OpenMovieDataBase Movie, Serie and Episode Browser

---
+++ First Quarterly/Unit 7/FilmDataBase/code.js
@@ -0,0 +1,67 @@
+let pageToSearch;
+let lookingFor;
+let end;
+
+function getMovies() {
+    let terms = $('#terms').val();
+    let type = $('#type').val();
+    pageToSearch = 1;
+    lookingFor = false;
+    end = false;
+    printMovies(terms, type, true);
+
+    let win = $(window);
+    win.scroll(function () {
+        // End of the document reached?
+        if ($(document).height() - win.height() - 150 <= win.scrollTop()) {
+            printMovies(terms, type, false);
+        }
+    });
+}
+
+function printMovies(terms, type, clear = false) {
+    if (lookingFor || end) return;
+
+    lookingFor = true;
+    $('.loading').show();
+
+    $.ajax({
+        url: "http://www.omdbapi.com/",
+        method: "get",
+        data: {"s": terms, "apikey": "91b35794", "type": type, "r": "json", "page": pageToSearch,},
+        dataType: "json",
+        success: function (data) {
+            ++pageToSearch;
+            styleMovies(data, clear);
+            $('.loading').hide();
+            lookingFor = false;
+        }
+    });
+}
+
+function styleMovies(jsonSearch, clear = false) {
+    if (clear) $('#results').empty();
+    if (jsonSearch['Search']) {
+        jsonSearch['Search'].forEach((element) => {
+            let eleBox = $('<div class="element"></div>')
+                .append($('<div class="text"></div>')
+                    .append($(`<div class="title">${element['Title']} <span class="year">(${element['Year']})</span></div>`))
+                );
+
+            if (element['Poster'] !== 'N/A') eleBox.append($(`<div class="img"><a href="http://www.imdb.com/title/${element['imdbID']}/"><img src="${element['Poster']}" /></a></div>`));
+            else eleBox.append($('<div class="img"><a href="http://www.imdb.com/title/${element[\'imdbID\']}/"><img src="http://www.interlog.com/~tfs/images/posters/TFSMoviePosterUnavailable.jpg" /></a></div>'));
+            $('#results').append(eleBox);
+        });
+    } else {
+        end = true;
+    }
+}
+
+window.addEventListener("load", () => {
+    $('#terms').keypress(function (e) {
+        if (e.which === 13) {
+            e.preventDefault();
+            getMovies();
+        }
+    });
+});
--- First Quarterly/Unit 7/Worksheet #3/codigo.js
+++ First Quarterly/Unit 7/Worksheet #3/codigo.js
@@ -1,9 +1,43 @@
-var var1 = window.prompt("Introduce un número");
-var var2 = window.prompt("Introduce otro número");
-console.log("El valor de la primera variable es " + var1);
-console.log("El valor de la segunda variable es " + var2);
+let createTable = () => {
+    let tableid = '#sixteen';
 
-console.log("La suma es: " + (+var1 + +var2));
-console.log("La resta es: " + (var1 - var2));
-console.log("La multiplicación es: " + (var1 * var2));
-console.log("La división es: " + (var1 / var2));
+    let eightrows = '<tr><td></td><td> </td><td> </td><td></td><td></td><td> </td><td> </td><td></td></tr>'.repeat(7);
+    $(tableid).find('tbody:last').append(eightrows);
+};
+
+let ej1 = () => {
+    let tableid = '#sixteen';
+
+    $(tableid + ' tr:nth-child(-n+2) td').css({'background-color': 'red'});
+    $(tableid + ' tr:nth-last-child(-n+2) td').css({'background-color': 'blue'});
+};
+
+let ej2 = () => {
+    let tableid = '#sixteen';
+
+    $(tableid + ' tr td:empty').css({'background-color': 'yellow'});
+};
+
+let ej7 = () => {
+    let tableid = '#sixteen';
+
+    $(tableid + ' tr td').animate({width: '25'}, 1000).animate({height: '25'}, 1000).animate({width: '50'}, 1000).animate({height: '50'}, 1000);
+};
+
+let ej7jump = () => {
+    let tableid = '#sixteen';
+    $(tableid + ' tr td').dequeue();
+};
+
+let ej7clear = () => {
+    let tableid = '#sixteen';
+    $(tableid + ' tr td').clearQueue();
+};
+
+
+window.onload = () => {
+    createTable();
+
+    ej2();
+    ej1();
+};
